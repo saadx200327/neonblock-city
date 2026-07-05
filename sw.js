c2131f879611ad21b301bc7fe49225784dc3cde4
saadx200327 <saadahmed0020@gmail.com>
2026-07-05 15:48:44 -0700

    Add static service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'neonblock-city-v14';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './firebase-backend.js', './manifest.webmanifest'];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});

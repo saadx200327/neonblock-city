0cfbbc3430cddd7078223d316f5ab284d1c98621
saadx200327 <saadahmed0020@gmail.com>
2026-07-06 14:50:03 -0700

    Add static service worker

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'neonblock-city-v2';
+const CORE = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './firebase-backend.js', './assets/icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin && !url.hostname.includes('unpkg.com')) return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});

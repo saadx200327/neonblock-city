15bba1d8c6890b9c1de0d02ae53ac4a83f5853fb
saadx200327 <saadahmed0020@gmail.com>
2026-07-07 13:50:40 -0700

    Add Netlify-ready service worker

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'neonblock-city-v16';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './firebase-backend.js', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((key) => key === CACHE_NAME ? null : caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match('./index.html'))));
+});

a1c54c63e9dd41e1c14496c9cfa6798ce0847acc
saadx200327 <saadahmed0020@gmail.com>
2026-07-11 11:50:21 -0700

    Refresh PWA cache for world safety update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'neonblock-city-';
-const CACHE_NAME = `${CACHE_PREFIX}v86`;
+const CACHE_NAME = `${CACHE_PREFIX}v87`;
 const MAX_RUNTIME_ENTRIES = 96;
 const CORE_ASSETS = [
   './',

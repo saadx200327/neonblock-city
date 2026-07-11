29b24b0e4f9a4064695d54ac029bd98259864d75
saadx200327 <saadahmed0020@gmail.com>
2026-07-11 13:47:05 -0700

    Refresh PWA cache for vehicle health update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'neonblock-city-';
-const CACHE_NAME = `${CACHE_PREFIX}v88`;
+const CACHE_NAME = `${CACHE_PREFIX}v89`;
 const MAX_RUNTIME_ENTRIES = 96;
 const CORE_ASSETS = [
   './',

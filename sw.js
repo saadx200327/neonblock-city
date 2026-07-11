abef71bde3af18a5395211f213ebd34d0bbf8dce
saadx200327 <saadahmed0020@gmail.com>
2026-07-11 05:49:24 -0700

    Refresh PWA cache for lifecycle brake fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'neonblock-city-';
-const CACHE_NAME = `${CACHE_PREFIX}v80`;
+const CACHE_NAME = `${CACHE_PREFIX}v81`;
 const MAX_RUNTIME_ENTRIES = 96;
 const CORE_ASSETS = [
   './',

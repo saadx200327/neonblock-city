5ff265a71297c3c99463ba70b62329d7f6c5a135
saadx200327 <saadahmed0020@gmail.com>
2026-07-11 16:49:56 -0700

    Refresh PWA cache for banking polish

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'neonblock-city-';
-const CACHE_VERSION = 'v90';
+const CACHE_VERSION = 'v91';
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
 const MAX_RUNTIME_ENTRIES = 96;
 const CORE_ASSETS = [

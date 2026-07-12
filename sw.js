0acbbf1b3c1161affa6389da4d7860c38df33d96
saadx200327 <saadahmed0020@gmail.com>
2026-07-11 22:49:56 -0700

    Refresh PWA cache for inventory update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'neonblock-city-';
-const CACHE_VERSION = 'v95';
+const CACHE_VERSION = 'v96';
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
 const MAX_RUNTIME_ENTRIES = 96;
 const CORE_ASSETS = [
